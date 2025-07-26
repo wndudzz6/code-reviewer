5a46780e7a5f1baf70b1b69e95735d30a1041997
O ju yeong <osmzxcvb@naver.com>
2025-07-26 14:11:22 +0900

    D드라이브 이동 후 깃 연결테스트

--- src/test/java/com/wndudzz6/codereviewer/service/CodeReviewerApplicationTests.java
+++ src/test/java/com/wndudzz6/codereviewer/service/CodeReviewerApplicationTests.java
@@ -3,7 +3,7 @@ package com.wndudzz6.codereviewer.service;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//D드라이브 이동 후 ci테스트
 @Disabled("CI환경에서 contextLoads 필요없음")
 class CodeReviewerApplicationTests {
 
