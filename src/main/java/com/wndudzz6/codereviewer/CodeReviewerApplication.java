18d1c9d2c897a027c3d4246307419af95e8145c6
O ju yeong <osmzxcvb@naver.com>
2025-07-29 14:39:44 +0900

    fix : JPA repository 스캔 for UserRepository

--- src/main/java/com/wndudzz6/codereviewer/CodeReviewerApplication.java
+++ src/main/java/com/wndudzz6/codereviewer/CodeReviewerApplication.java
@@ -2,8 +2,10 @@ package com.wndudzz6.codereviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.wndudzz6.codereviewer.repository")
 public class CodeReviewerApplication {
 
     public static void main(String[] args) {
