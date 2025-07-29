de33570dde5842c54dbf49cd486154e834656ebe
O ju yeong <osmzxcvb@naver.com>
2025-07-29 15:14:46 +0900

    fix : 구현체 주입 오류 수정 인터페이스 주입 AICodeReviewerService

--- src/main/java/com/wndudzz6/codereviewer/controller/SubmissionController.java
+++ src/main/java/com/wndudzz6/codereviewer/controller/SubmissionController.java
@@ -3,6 +3,7 @@ package com.wndudzz6.codereviewer.controller;
 import com.wndudzz6.codereviewer.domain.Submission;
 import com.wndudzz6.codereviewer.dto.SubmissionRequest;
 import com.wndudzz6.codereviewer.dto.SubmissionResponse;
+import com.wndudzz6.codereviewer.service.AICodeReviewerService;
 import com.wndudzz6.codereviewer.service.GptCodeReviewerService;
 import com.wndudzz6.codereviewer.service.SubmissionService;
 import lombok.RequiredArgsConstructor;
@@ -17,13 +18,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class SubmissionController {
 
-    private final GptCodeReviewerService gptCodeReviewerService;
+    private final AICodeReviewerService aiCodeReviewerService;
     private final SubmissionService submissionService;
 
     @PostMapping
     public ResponseEntity<SubmissionResponse> submit(@RequestParam Long userId, @RequestBody Map<String, String> body) {
         String code = body.get("code");
-        Submission saved = gptCodeReviewerService.createSubmissionWithReview(code, userId);
+        Submission saved = aiCodeReviewerService.createSubmissionWithReview(code, userId);
         return ResponseEntity.ok(SubmissionResponse.from(saved));
     }
 
--- src/main/java/com/wndudzz6/codereviewer/service/AICodeReviewerService.java
+++ src/main/java/com/wndudzz6/codereviewer/service/AICodeReviewerService.java
@@ -1,6 +1,7 @@
 package com.wndudzz6.codereviewer.service;
 
 import com.wndudzz6.codereviewer.domain.Review;
+import com.wndudzz6.codereviewer.domain.Submission;
 import com.wndudzz6.codereviewer.dto.ReviewRequest;
 import com.wndudzz6.codereviewer.dto.SubmissionRequest;
 
@@ -9,4 +10,7 @@ public interface AICodeReviewerService {
     //AI한테 코드 리뷰 요청 -> ReviewRequest로 변환
     ReviewRequest review(SubmissionRequest request);
     ReviewRequest createReviewForSubmission(SubmissionRequest request);
+
+    //Submission + Review를 동시에 생성
+    Submission createSubmissionWithReview(String code, Long userId);
 }
